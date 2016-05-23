c0ebdd348181398789c71377fc97167355772861
Liz Favre <egfavre@gmail.com>
2016-05-23 12:02:44 -0400

    Handle exceptions

---
+++ src/com/egfavre/Main.java
@@ -0,0 +1,82 @@
+package com.egfavre;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    //make methods for refactoring
+    public static void createItem(Scanner scanner, ArrayList<ToDoItem> items) {
+        System.out.println("Enter your to do item:");
+        String text = scanner.nextLine();
+        ToDoItem item = new ToDoItem(text, false);
+        items.add(item);
+    }
+
+    public static void toggleItem(Scanner scanner, ArrayList<ToDoItem> items) {
+        System.out.println("Enter the number of the item you wish to toggle");
+        String numStr = scanner.nextLine();
+        try {
+            int num = Integer.valueOf(numStr);
+            ToDoItem tempItem = items.get(num - 1);
+            tempItem.isDone = !tempItem.isDone;
+        } catch (NumberFormatException e) {
+            System.out.println("You didn't type a number.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Number isn't valid.");
+        }
+        //already in array list, do not need to add back
+    }
+
+    public static void listItems(ArrayList<ToDoItem> items) {
+        int i = 1;
+        //List items
+        for (ToDoItem toDoItem : items) {
+            String checkbox = "[ ]";
+            if (toDoItem.isDone) {
+                checkbox = "[x]";
+            }
+            //simplest way:System.out.println(toDoItem.text);
+            //but we want index
+            System.out.println(checkbox + " " + i + ". " + toDoItem.text);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ToDoItem> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        //create new item
+        //toggle done
+        //print current list
+
+        //infinite loop
+        while (true) {
+            System.out.println("1. Create new Item");
+            System.out.println("2. Toggle Item");
+            System.out.println("3. List Items");
+
+            //read
+            String option = scanner.nextLine();
+
+            //if or switch statment
+            switch (option) {
+                case "1":
+                    //create new item
+                    createItem(scanner, items);
+                    break;
+                case "2":
+                    //toggle item
+                    toggleItem(scanner, items);
+                    break;
+                case "3":
+                    listItems(items);
+                    break;
+
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+    }
+}
---
+++ src/com/egfavre/ToDoItem.java
@@ -0,0 +1,17 @@
+package com.egfavre;
+
+/**
+ * Created by user on 5/23/16.
+ */
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    //need constructor and instantiate
+
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}

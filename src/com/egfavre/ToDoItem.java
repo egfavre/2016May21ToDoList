0d3e1037f619f0ffc60a22080b19efdbaa61dcf8
Liz Favre <egfavre@gmail.com>
2016-06-13 12:15:39 -0400

    add database

--- src/com/egfavre/ToDoItem.java
+++ src/com/egfavre/ToDoItem.java
@@ -4,6 +4,7 @@ package com.egfavre;
  * Created by user on 5/23/16.
  */
 public class ToDoItem {
+    int id;
     String text;
     boolean isDone;
 
@@ -11,6 +12,12 @@ public class ToDoItem {
 
 
     public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
